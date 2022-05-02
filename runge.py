6ec691268eb1a26b4cbaf79a4314991c499d267e
Sergei Sheverev <sergei_sheverev@mail.ru>
2022-05-02 21:56:59 +0300

    adapted output value

--- runge.py
+++ runge.py
@@ -10,19 +10,21 @@ def f(t, x, y):
     return (exp(y) - 1 + p2*(exp(x)-1)/(p2 + exp(2*x))*exp(y), p1*(1 - exp(x)) - p1*p2*(exp(x)-1)/(p2 + exp(2*x))*exp(x))
 #(x0, y0) --- Cauchy, f = y' --- function, iters --- number of iterations, h - is step
 def RungeKutt(t0, x0, y0, f, iters, h):
-    x = [x0]
-    y = [y0]
+    res = [[x0, y0]]
+
     t = t0
     for i in range(0, iters):
-        k1 = f(t, x[-1], y[-1])
-        k2 = f(t + h/2, x[-1] + h/2*k1[0], y[-1] + h/2*k1[1])
-        k3 = f(t+ h/2, x[-1]+ h/2*k2[0] , y[-1] + h/2*k2[1])
-        k4  =f(t + h, x[-1]+ h*k3[0], y[-1] + h*k3[1])
+        k1 = f(t, res[-1][0], res[-1][1])
+        k2 = f(t + h/2, res[-1][0] + h/2*k1[0], res[-1][1] + h/2*k1[1])
+        k3 = f(t+ h/2, res[-1][0]+ h/2*k2[0] , res[-1][1] + h/2*k2[1])
+        k4  =f(t + h, res[-1][0]+ h*k3[0], res[-1][1] + h*k3[1])
         t += h
-        x.append(x[-1] +  h/6*(k1[0]+2*k2[0]+2*k3[0] + k4[0]))
-        y.append(y[-1] +  h/6*(k1[1]+2*k2[1]+2*k3[1] + k4[1]))
-    return (x, y)
-x, y = RungeKutt(0,0.5,0.55,f,50000,0.01)
-plt.scatter(x,y, label="runge", s=0.3);
+        
+        x = res[-1][0] +  h/6*(k1[0]+2*k2[0]+2*k3[0] + k4[0])
+        y = res[-1][1] +  h/6*(k1[1]+2*k2[1]+2*k3[1] + k4[1])
+        res.append([x, y])
+    return res
+data = RungeKutt(0,0.5,0.55,f,50000,0.01)
+plt.scatter([i[0] for i in data ],[i[1] for i in data], label="runge", s=0.3);
 plt.legend()
 plt.show()
